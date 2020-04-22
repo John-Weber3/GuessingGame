0498b83a69763d07c943f464e175fcf3b3d84708
John-Weber3 <jweb1997@optonline.net>
2020-04-22 23:43:34 +0000

    Completed Guessing Game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
+# File: guessinggame.sh
+# Author: John Weber
+
+# $1 is for response and $2 is for number_files
+function compare_values {
+   if [[ $1 -eq $2 ]]
+  then
+    echo "That is correct"
+    correct_number="yes"
+  elif [[ $1 -gt $2 ]]
+  then
+    echo "Too high"
+  else
+    echo "Too low"
+  fi
+}
+
 
 # Number of files in dir
 number_files=$(ls | wc -w)
 
-echo "Guess the number of files in this directory"
-read response
 
-if [[ $response -eq $number_files ]]
-then
- echo "That is correct"
-elif [[ $response -gt $number_files ]]
-then
- echo "Too high"
-else
- echo "Too low"
-fi
+correct_number="no"
+while [[ $correct_number != "yes" ]]
+do
+  echo "Guess the number of files in this directory"
+  read response 
+  compare_values $response $number_files
+done
